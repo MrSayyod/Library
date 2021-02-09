51b363f16128bef7eacb3c5b49581ee7704d1608
bini-i <binyam.sh1@gmail.com>
2021-02-09 21:35:19 +0300

    Update addBookToLibrary and Add displayBooks methods
    - update addBookToLibrary to push books to myLibrary array
    - displayBooks method display books in library

--- script.js
+++ script.js
@@ -5,19 +5,29 @@ let book = {
   status: true
 }
 
-let myArray = []
-function Book() {
+let myLibrary = []
+function Book(title, author, pages, status) {
   this.title = title
   this.author = author
   this.pages = pages
   this.status = status
 }
 
-title = prompt("What is the title of the new book?")
-author = prompt("Who is the author of the book?")
-pages = prompt("How many pages do the new book have?")
-status = confirm("Did you read the book until the last page?")
-
 function addBookToLibrary() {
-  myArray.push(new Book())
+  title = prompt("What is the title of the new book?")
+  author = prompt("Who is the author of the book?")
+  pages = prompt("How many pages do the new book have?")
+  status = confirm("Did you read the book until the last page?")
+  myLibrary.push(new Book(title, author, pages, status));
+}
+
+// addBookToLibrary();
+
+function displayBooks(){
+  for(let i = 0; i < myLibrary.length; i++){
+    myLibrary[i].title;
+    myLibrary[i].author;
+    myLibrary[i].pages;
+    myLibrary[i].status;
+  }
 }
