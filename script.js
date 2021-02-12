a98e1c18870615905baeaa50cbf43aa9f217ca4f
MrSayyod <ssayyod@gmail.com>
2021-02-12 16:57:28 +0500

    Fix linter errors

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-let myLibrary = [];
+const myLibrary = [];
 
-const body = document.querySelector('body')
+const body = document.querySelector('body');
 
-//Book constructor
+// Book constructor
 function Book(title, author, pages, status) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.status = status
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 }
-
-Book.prototype.toggleRead = function() {
-  console.log("From toggel read");
-  console.log(this);
+function toggle() {
   this.status = !this.status;
-};
-
-function addBookToLibrary(title, author, pages, status) {
-  myLibrary.push(new Book(title, author, pages, status));
-  displayBooks();
 }
-
+Book.prototype.toggleRead = toggle;
 
 function displayBooks() {
   if (document.querySelector('table')) {
@@ -44,8 +36,8 @@ function displayBooks() {
 
   body.appendChild(table);
 
-  for (let i = 0; i < myLibrary.length; i++) {
-    const row = document.createElement('tr')
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const row = document.createElement('tr');
 
     const title = document.createElement('td');
     title.textContent = myLibrary[i].title;
@@ -66,31 +58,35 @@ function displayBooks() {
     const deleteCol = document.createElement('td');
     const deleteBtn = document.createElement('button');
     deleteBtn.setAttribute('data-index', i);
-    deleteBtn.textContent = "Delete";
-    deleteCol.appendChild(deleteBtn)
+    deleteBtn.textContent = 'Delete';
+    deleteCol.appendChild(deleteBtn);
     deleteBtn.onclick = (event) => {
       myLibrary.splice(event.target.dataset.index, 1);
-      table.removeChild(row)
+      table.removeChild(row);
     };
-    row.appendChild(deleteCol)
+    row.appendChild(deleteCol);
 
-    const toggleReadCol = document.createElement('td')
-    const toggleReadBtn = document.createElement('button')
-    if(myLibrary[i].status){
-      toggleReadBtn.textContent = 'UnRead'
-    }else{
-      toggleReadBtn.textContent = 'Read'
+    const toggleReadCol = document.createElement('td');
+    const toggleReadBtn = document.createElement('button');
+    if (myLibrary[i].status) {
+      toggleReadBtn.textContent = 'UnRead';
+    } else {
+      toggleReadBtn.textContent = 'Read';
     }
-    toggleReadBtn.onclick = ()=> {
-      console.log(myLibrary[i]);
+    toggleReadBtn.onclick = () => {
       myLibrary[i].toggleRead();
       displayBooks();
-    }
-    toggleReadCol.appendChild(toggleReadBtn)
-    row.appendChild(toggleReadCol)
+    };
+    toggleReadCol.appendChild(toggleReadBtn);
+    row.appendChild(toggleReadCol);
 
     table.appendChild(row);
   }
+}
+
+function addBookToLibrary(title, author, pages, status) {
+  myLibrary.push(new Book(title, author, pages, status));
+  displayBooks();
 }
 
 displayBooks();
@@ -98,50 +94,51 @@ displayBooks();
 const newBook = document.querySelector('.btn');
 
 newBook.onclick = () => {
-  const form = document.createElement('form')
-  const labelForTitle = document.createElement('label')
-  labelForTitle.setAttribute('for', 'title')
-  labelForTitle.textContent = 'Title'
-  const titleForBook = document.createElement('input')
-  titleForBook.setAttribute('type', 'text')
-  titleForBook.setAttribute('id', 'title')
-  labelForTitle.appendChild(titleForBook)
-  form.appendChild(labelForTitle)
+  const form = document.createElement('form');
+  const labelForTitle = document.createElement('label');
+  labelForTitle.setAttribute('for', 'title');
+  labelForTitle.textContent = 'Title';
+  const titleForBook = document.createElement('input');
+  titleForBook.setAttribute('type', 'text');
+  titleForBook.setAttribute('id', 'title');
+  labelForTitle.appendChild(titleForBook);
+  form.appendChild(labelForTitle);
 
-  const labelForAuthor = document.createElement('label')
-  labelForAuthor.setAttribute('for', 'author')
-  labelForAuthor.textContent = 'Author'
-  const authorForBook = document.createElement('input')
-  authorForBook.setAttribute('type', 'text')
-  authorForBook.setAttribute('id', 'author')
-  labelForAuthor.appendChild(authorForBook)
-  form.appendChild(labelForAuthor)
+  const labelForAuthor = document.createElement('label');
+  labelForAuthor.setAttribute('for', 'author');
+  labelForAuthor.textContent = 'Author';
+  const authorForBook = document.createElement('input');
+  authorForBook.setAttribute('type', 'text');
+  authorForBook.setAttribute('id', 'author');
+  labelForAuthor.appendChild(authorForBook);
+  form.appendChild(labelForAuthor);
 
-  const labelForPages = document.createElement('label')
-  labelForPages.setAttribute('for', 'pages')
-  labelForPages.textContent = 'Number of pages'
-  const pagesForBook = document.createElement('input')
-  pagesForBook.setAttribute('type', 'text')
-  pagesForBook.setAttribute('id', 'pages')
-  labelForPages.appendChild(pagesForBook)
-  form.appendChild(labelForPages)
+  const labelForPages = document.createElement('label');
+  labelForPages.setAttribute('for', 'pages');
+  labelForPages.textContent = 'Number of pages';
+  const pagesForBook = document.createElement('input');
+  pagesForBook.setAttribute('type', 'text');
+  pagesForBook.setAttribute('id', 'pages');
+  labelForPages.appendChild(pagesForBook);
+  form.appendChild(labelForPages);
 
-  const labelForReadStatus = document.createElement('label')
-  labelForReadStatus.setAttribute('for', 'status')
-  labelForReadStatus.textContent = "Click if you have finished the book"
-  const statusForBook = document.createElement('input')
-  statusForBook.setAttribute('type', 'checkbox')
-  statusForBook.setAttribute('id', 'status')
-  labelForReadStatus.appendChild(statusForBook)
-  form.appendChild(labelForReadStatus)
+  const labelForReadStatus = document.createElement('label');
+  labelForReadStatus.setAttribute('for', 'status');
+  labelForReadStatus.textContent = 'Click if you have finished the book';
+  const statusForBook = document.createElement('input');
+  statusForBook.setAttribute('type', 'checkbox');
+  statusForBook.setAttribute('id', 'status');
+  labelForReadStatus.appendChild(statusForBook);
+  form.appendChild(labelForReadStatus);
 
-  const submit = document.createElement('input')
-  submit.setAttribute('type', 'button')
-  submit.setAttribute('value', "Add to the Library")
-  form.appendChild(submit)
+  const submit = document.createElement('input');
+  submit.setAttribute('type', 'button');
+  submit.setAttribute('value', 'Add to the Library');
+  form.appendChild(submit);
   submit.onclick = () => {
-    addBookToLibrary(titleForBook.value, authorForBook.value, pagesForBook.value, statusForBook.checked)
-    body.removeChild(form)
-  }
-  body.appendChild(form)
-}
+    addBookToLibrary(titleForBook.value, authorForBook.value,
+      pagesForBook.value, statusForBook.checked);
+    body.removeChild(form);
+  };
+  body.appendChild(form);
+};
